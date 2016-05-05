5eb89607ed0ac0519f019041ba9c0d47c9df787c
Alexandre Seidy <seidy.alexandre@gmail.com>
2016-05-05 15:31:27 -0300

    unfinished update

--- Lab8/Exp8_Part1.vhd
+++ Lab8/Exp8_Part1.vhd
@@ -10,18 +10,18 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity Exp7_Part1 is
-	port (
-			SW: in std_logic_vector(17 downto 0);
-			LEDR: out std_logic_vector(8 downto 0);
-			LEDG: out std_logic_vector(7 downto 0);
-			KEY: in std_logic_vector(1 downto 0);
-			CLOCK_50: in std_logic
-		  );
-end Exp7_Part1;
+entity Exp8_Part1 is
+    port (
+            SW: in std_logic_vector(17 downto 0);
+            LEDR: out std_logic_vector(8 downto 0);
+            LEDG: out std_logic_vector(7 downto 0);
+            KEY: in std_logic_vector(1 downto 0);
+            CLOCK_50: in std_logic
+          );
+end Exp8_Part1;
 
-architecture behavior OF Exp7_Part1 is
-	component counter_k_init
+architecture behavior OF Exp8_Part1 is
+    component counter_k_init
         generic (
             -- Change 'n' for larger (or shorter) maximum values for 'k'
             n : natural := 8
@@ -42,7 +42,7 @@ architecture behavior OF Exp7_Part1 is
             dec_out: OUT std_logic_vector(6 downto 0)
         );
     end component decoder_7segment_hex;
-	component timer
+    component timer
         generic (
             duration : natural := 8
         );
@@ -55,23 +55,36 @@ architecture behavior OF Exp7_Part1 is
 
 
     signal clk_sec: std_logic;
-	signal car_waiting: std_logic;
-	signal car_waiting: std_logic;
-	signal reset, reset_n: std_logic;
-	type traffic_status is (R, G, Y);
-	signal TL1, TL2: traffic_status;
-	signal reset_yellow, completed_yellow: std_logic;
+    signal car_waiting: std_logic;
+    signal car_waiting: std_logic;
+    signal reset, reset_n: std_logic;
+    type traffic_status is (R, G, Y);
+    signal TL1, TL2: traffic_status;
+    signal reset_yellow, completed_yellow: std_logic;
 
 begin
 
-	reset <= not(KEY(0));
-	reset_n <= KEY(0);
+    reset <= not(KEY(0));
+    reset_n <= KEY(0);
     seconds_counter: counter_k_init
         generic map(n => 32)
         port map(k => 50000000, clock => CLOCK_50, reset_n => reset_n, Q => s_base, init => 0, complete => clk_sec);
 
     timer_yellow_light: timer
-    	generic map(duration => 1)
+        generic map(duration => 1)
         port map(clk_sec, reset_yellow, completed_yellow);
+
+    process(TL1)
+    begin
+        case(TL1) is
+            when R
+
+            when G
+
+            when Y
+                
+        end case;
+
+    end process;
 
 end behavior;
